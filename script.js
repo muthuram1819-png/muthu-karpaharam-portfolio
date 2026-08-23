55bd7ae6d59082c4ff97b327c2dd7688453c54a9
Muthu Arun <ksmmuthuram2007@gmail.com>
2026-08-23 22:13:12 +0530

    Upgrade portfolio interface and dark mode

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "☀️";
+    } else {
+        themeToggle.textContent = "🌙";
+    }
+
+});
