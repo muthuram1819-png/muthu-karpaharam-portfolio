93fa664bd250e182a5d7356df2b969667d8157d6
Muthu Arun <ksmmuthuram2007@gmail.com>
2026-08-23 10:28:03 +0530

    add cgpa

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-.skills-container {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 18px;
-    margin-top: 30px;
-}
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
 
-.skills-container div {
-    background: white;
-    min-width: 130px;
-    padding: 18px 22px;
-    border-radius: 10px;
-    box-shadow: 0 3px 12px rgba(0, 0, 0, 0.1);
-    font-weight: bold;
-    transition: 0.3s ease;
-    border-bottom: 3px solid #2563eb;
-}
+window.addEventListener("scroll", () => {
+    let current = "";
 
-.skills-container div:hover {
-    transform: translateY(-7px);
-    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.15);
-}
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
